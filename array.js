b9aa86dc95b641b9a1265b857ce3bf391d9fc01e
siddengineer <siddharthpatil7381@gmail.com>
2025-02-18 16:21:43 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,106 @@
+//Among the superior try to be the greatest.........
+
+
+
+//array simple
+
+// let fruits = ["apple","banana","cherry"]
+// console.log(fruits)
+
+//mixed array
+
+// let mixedarray = ["apple",25,true,null]
+// console.log(mixedarray)
+
+
+//push 
+// let fruits = ["apple","banana"]
+// fruits.push("orange")
+// console.log(fruits)
+
+
+//pop
+// let fruits = ["apple","banana","orange"]
+// fruits.pop()
+// console.log(fruits)
+
+
+//shift
+// let fruits = ['apple',"banana","cherry"]
+// let firstFirst = fruits.shift()
+// console.log(fruits)
+
+
+//unshift
+// let fruits = ["banana","cherry"]
+// fruits.unshift("apple")
+// console.log(fruits)
+
+
+//foreach()
+// let fruits = ["apple","banana","cherry"]
+// fruits.forEach(function(fruit){
+//     console.log(fruits);
+// });
+
+
+//map()
+
+// let numbers = [1,2,3]
+// let doubleNumbers = numbers.map(num => num*2);
+// console.log(doubleNumbers);
+
+//filter()
+// let numbers = [1, 2, 3, 4, 5];
+// let evenNumbers = numbers.filter(num => num % 2 === 0);
+// console.log(evenNumbers);  // [2, 4]
+
+
+//reduce()
+// let numbers = [1, 2, 3, 4];
+// let sum = numbers.reduce((acc, num) => acc + num, 0);
+// console.log(sum);  // 10
+
+//concat
+// let fruits = ['apple', 'banana'];
+// let moreFruits = ['cherry', 'date'];
+// let combined = fruits.concat(moreFruits);
+// console.log(combined);  // ['apple', 'banana', 'cherry', 'date']
+
+//slice()
+// let fruits = ['apple', 'banana', 'cherry', 'date'];
+// let sliced = fruits.slice(1, 3);
+// console.log(sliced);  // ['banana', 'cherry']
+
+// splice()
+// let fruits = ['apple', 'banana', 'cherry'];
+// fruits.splice(1, 1, 'date', 'elderberry');  // Removes 1 item at index 1 and adds two new items
+// console.log(fruits);  // ['apple', 'date', 'elderberry', 'cherry']
+
+
+//destructuring array
+// let fruits = ['apple', 'banana', 'cherry'];
+// let [first, second] = fruits;
+// console.log(first);  // 'apple'
+// console.log(second); // 'banana'
+
+// spread operatr(...)
+
+// let fruits = ['apple', 'banana', 'cherry'];
+// let moreFruits = [...fruits, 'date', 'elderberry'];
+// console.log(moreFruits);  // ['apple', 'banana', 'cherry', 'date', 'elderberry']
+
+//array ferom
+// let str = 'hello';
+// let arr = Array.from(str);  // ['h', 'e', 'l', 'l', 'o']
+
+//find()
+// let numbers = [1, 2, 3, 4, 5];
+// let found = numbers.find(num => num > 3);
+// console.log(found);  // 4
+
+//sort()
+// let numbers = [5, 3, 8, 1];
+// numbers.sort();  // [1, 3, 5, 8]
+
+
